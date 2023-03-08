cb832f79b66bcfb19078f6b19f2cd79e0d16e5e3
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2023-03-08 23:08:23 +0530

    History Page component

---
+++ client/src/components/History/History.js
@@ -0,0 +1,148 @@
+import React, { useEffect, useState } from 'react'
+import LoadingBar from 'react-top-loading-bar'
+import Sidenav from '../global/Sidebar'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from 'react-router-dom';
+import {Helmet} from "react-helmet";
+
+const History = () => {
+    const [progress, setProgress] = useState(0);
+    const [notesList, setNotesList] = useState([]);
+
+    const navigate = useNavigate();
+
+    const getDeletedNotes = async (authtoken) => {
+        setProgress(20);
+        const response = await fetch('http://localhost:8181/api/notes/bin', {
+
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'auth-token': authtoken
+            }
+        });
+
+        setProgress(50);
+        
+        const json = await response.json();
+
+        setProgress(70);
+
+        setNotesList(json);
+
+        setProgress(100);
+    }
+
+
+    useEffect(() => {
+      const authtoken = sessionStorage.getItem('auth-token');
+      if (authtoken) {
+        getDeletedNotes(authtoken);
+      }
+      else {
+        navigate('/login');
+      }
+    }, [])
+    
+
+
+  const convertToMonthName = (num) => {
+    var months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+
+    return months[num];
+  }
+
+
+
+  const unArchive = async (id) => {
+    const authtoken = sessionStorage.getItem('auth-token');
+    const response = await fetch(`http://localhost:8181/api/notes/unarchive/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': authtoken
+      }
+    });
+
+    const json = await response.json();
+    toast.success(json.success);
+    // setNotesList(notesList.filter())
+    await getDeletedNotes(authtoken);
+  }
+
+
+  const deletePermanently = async (id) => {
+    if (window.confirm('Are You sure you want to delete this Note Permanently?')) {
+      const authtoken = sessionStorage.getItem('auth-token');
+      const response = await fetch(`http://localhost:8181/api/notes/deletenotepermanently/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'auth-token': authtoken
+        }
+      });
+      const json = await response.json();
+      toast.success(json.success);
+      await getDeletedNotes(authtoken);
+    }
+  }
+
+
+  return (
+    <>
+    <Helmet>
+        <title>History | Convin</title>
+        <meta name="description" content="Convin. Clear Or Restore Your Files from Bin." />
+    </Helmet>
+    <LoadingBar
+        color='#f11946'
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
+    <Sidenav />
+    <section className="home">
+      <div className="text">History</div>
+
+      <div className="wrapper">
+
+
+        {notesList && notesList.map((note) => {
+            const dateStu = note.createdAt;
+            return (
+                <li id={note._id} key={note._id} className="note">
+                    <div className="details">
+                        <iframe
+                            width="460"
+                            height="350"
+                            src={note.youtubeUrl}
+                            title="YouTube video player"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowFullScreen
+                          ></iframe>
+                        <p>{note.title}</p>
+                        <span>{note.description}</span>
+                    </div>
+                    <div className="bottom-content">
+                        <span>{convertToMonthName(new Date(dateStu).getMonth()) + " " + new Date(dateStu).getDate().toString() + ", " + new Date(dateStu).getFullYear()}</span>
+                        <div onClick={() => unArchive(note._id)} id={`settings-${note._id}`} className="settings">
+                        <i class="fa-solid fa-inbox"></i>
+                        </div>
+                        <div onClick={() => deletePermanently(note._id)} id={`settings-${note._id}`} className="settings">
+                        <i class="fa-solid fa-trash"></i>
+                        </div>
+                    </div>
+                </li>
+            );
+        })}
+
+
+      </div>
+      <ToastContainer toastStyle={{ backgroundColor: "#202d40", color: 'white' }} />
+    </section>
+    </>
+  )
+}
+
+export default History
