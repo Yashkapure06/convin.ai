acd64f554ecbd071f7cd5fd4c87e10b16ba5f03c
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2023-03-08 23:15:53 +0530

    Created Models for Notes and User

---
+++ server/models/Notes.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
+
+
+const NoteSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    youtubeUrl:{
+        type: String,
+        required: true
+    },
+    authorId: {
+        type: String,
+        required: true
+    },
+    authorUsername: {
+        type: String,
+        required: true
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model('note', NoteSchema);
